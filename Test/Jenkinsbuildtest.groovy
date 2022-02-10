d7e404631cb7b0c1fc7404d3407fd092b5d6eb35
Srikanth.Ancha <srik2434@gmail.com>
2022-02-10 21:58:35 +0530

    newfile added

---
+++ Test/Jenkinsbuildtest.groovy
@@ -0,0 +1,28 @@
+pipeline{
+    agent any
+        stages {
+            stage ("Clone-branch"){
+                steps{
+                    println "clone the repositry into local"
+
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/sriancha/GitToday.git']]])
+                    sh "ls -lart ./*"
+                }
+            }
+            stage ("Build"){
+               steps{
+                   println " Build the job"
+                   sh "mvn clean package"
+               } 
+            }
+            stage ("Upload"){
+                steps{
+                    prnitln " depoly the code into branch"
+
+                    sh " ls -lart"
+                    sh "aws s3 cp target/hello-*.war s3://pipelineart "
+                }
+            }
+        }
+    
+}
